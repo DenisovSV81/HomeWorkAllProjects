c842493eb3ddf0c69eb1f2f1697f448b7f74a9c3
SergeyDenisov <denisov.sv81@gmail.com>
2022-07-01 15:12:24 +0300

    First try

--- C#/HW2/Program.cs
+++ C#/HW2/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 10 Принимает двухзначное число и выдает максимальную цифру
+
+int number = new Random().Next(10,100);
+int findBiggestNumber(int arg)
+{
+    Console.WriteLine("Current random number is " + number);
+    int firstDigit = arg / 10;
+    int secondDigit = arg % 10;
+    int max;
+    if (firstDigit > secondDigit) max = firstDigit;
+    else max = secondDigit;
+    return max;
+}
+int result = findBiggestNumber(number);
+Console.WriteLine("Max number is " + result);
