74fa2ea59497ac9c1b53410e202a8d39af3214e6
SergeyDenisov <denisov.sv81@gmail.com>
2022-07-01 14:47:56 +0300

    HW 1 Done

--- C#/HW1/Program.cs
+++ C#/HW1/Program.cs
@@ -1,2 +1,67 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 4
+/*
+Console.Write("Введите число 1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 3: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+int max = number1;
+if (number2 > max)
+{
+    max = number2;
+}
+if (number3 > max)
+{
+    max = number3;
+}
+Console.WriteLine("Максимальное число: " + max);
+*/
+
+//Задача 2
+/*
+Console.Write("Введите число 1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 == number2)
+{
+    Console.WriteLine("Числа равны");
+}
+else if (number1 > number2)
+{
+    Console.WriteLine("Число " + number1 + " больше, чем число " + number2);
+}
+else
+{
+    Console.WriteLine("Число " + number2 + " больше, чем число " + number1);
+} 
+*/
+
+//Задача 6
+/*
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 0)
+{
+    Console.WriteLine("Число четное");
+}
+else
+{
+    Console.WriteLine("Число нечетное");
+} 
+*/
+
+//Задача 8
+/*
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write(number + " -> ");
+for(int count = 1; count <= number; count ++)
+{
+    if (count % 2 == 0)
+    {
+    Console.Write(count + ", ");
+    }
+}
+*/
