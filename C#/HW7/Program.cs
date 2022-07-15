feb5ae3e0be9ff26c3fac33050ad5b6358851694
SergeyDenisov <denisov.sv81@gmail.com>
2022-07-15 20:09:35 +0300

    first task done

--- C#/HW7/Program.cs
+++ C#/HW7/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 47 Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+double[,] Random2dArray(int a, int b)
+{
+    while (a <= 0)
+    {
+        Console.Write("Невозможно построить двумерный массив с заданным количеством строк. Пожалуйста, введите новое значение: ");
+        a = Convert.ToInt32(Console.ReadLine());
+    }
+
+    while (b <= 0)
+    {
+        Console.Write("Невозможно построить двумерный массив с заданным количеством столбцов. Пожалуйста, введите новое значение: ");
+        b = Convert.ToInt32(Console.ReadLine());
+    }
+    
+    double[,] new2dArray = new double[a, b];
+
+    for (int i = 0; i < a; i++)
+        for (int j = 0; j < b; j++)
+            new2dArray[i, j] = (new Random().Next(-10, 11)) + new Random().NextDouble();
+
+    return new2dArray;
+}
+
+void Show2dArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Math.Round(array[i, j], 1);
+            Console.Write($"{array[i, j]}  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Write("Введите количество строк двумерного массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов в двумерном массиве: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[,] my2dArray = Random2dArray(m, n);
+
+Console.WriteLine("Сгенерированный двумерный массив:");
+
+Show2dArray(my2dArray);
